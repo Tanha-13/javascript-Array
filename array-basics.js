a4d62197851234fbd8631a881449993cb28566c4
Tanha <nusrattanha897@gmail.com>
2024-01-22 22:57:03 +0600

    basic array methods

--- array-basics.js
+++ array-basics.js
@@ -11,11 +11,11 @@ const arr3 = new Array();
  * info-2: arrays are resizable
  * info-3: can contain a mix of different data types.
  * info-4: start index from 0. 
- * info-5: 
  * 
  */
 // todo: shallow copies vs deep copies
-const myArr = [0,1,5,6,3,8,9];
-console.log(typeof myArr); //object
-console.log(myArr[0]); // accessing array in javascript
-console.log(myArr['one']); //not the proper way to access array elements.
+
+console.log(typeof arr2); //object
+console.log(Array.isArray(arr2));
+console.log(arr2[0]); // accessing array in javascript
+console.log(arr2['one']); //not the proper way to access array elements.
---
+++ array-methods.js
@@ -0,0 +1,56 @@
+const myArr = [0,1,5,6,3,8,9];
+
+// accessing array elements using index
+// console.log(myArr[0]); // will show the first element in the array
+
+// basic methods of array in javascript
+// topic: push
+myArr.push(7);
+myArr.push(10);
+// console.log(myArr);
+
+// topic: pop
+myArr.pop();
+// console.log(myArr);
+
+
+//? optimization problem for unshift and shift methods. not recommended
+// topic: unshift
+myArr.unshift(0);
+// console.log(myArr);
+
+// topic: shift
+myArr.shift();
+// console.log(myArr);
+
+// topic: includes
+// console.log(myArr.includes(9));
+
+// topic: indexOf
+// console.log(myArr.indexOf(89)); // -1. when the element is not present in the array, it returns its index as -1.
+// console.log(myArr.indexOf(9));
+
+// topic: join
+// info: join method returns a string
+// const newArr = myArr.join(); // returns with comma separator
+const newArr = myArr.join(' '); // returns with space separator
+
+// console.log(myArr);
+// console.log(newArr);
+//info: the main difference is type.
+// console.log(typeof myArr);  // object
+// console.log(typeof newArr); // string
+
+
+// topic: slice 
+console.log('A ', myArr);
+const sliceArray = myArr.slice(1,3);
+console.log(sliceArray);
+console.log('B ', myArr);
+
+// topic: splice
+const spliceArray = myArr.splice(1,3);
+console.log(spliceArray);
+console.log('C ', myArr);
+
+// todo: slice vs splice
