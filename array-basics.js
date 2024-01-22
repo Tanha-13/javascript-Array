bdcfdc481bbe24800dd8b0bf388486b572003f52
Tanha <nusrattanha897@gmail.com>
2024-01-22 22:11:23 +0600

    basics of array in javascript

---
+++ array-basics.js
@@ -0,0 +1,15 @@
+// declaring array
+// 1st
+/**
+ * info-1: Single variable, multiple elements(values).
+ * info-2: arrays are resizable
+ * info-3: can contain a mix of different data types.
+ * info-4: start index from 0. 
+ * info-5: 
+ * 
+ */
+// todo: shallow copies vs deep copies
+const myArr = [0,1,5,6,3,8,9];
+console.log(typeof myArr); //object
+console.log(myArr[0]); // accessing array in javascript
+console.log(myArr['one']); //not the proper way to access array elements.
