e20ce3d0978f0a8e05dd810826cd3118de7c5362
Tanha <nusrattanha897@gmail.com>
2024-02-02 22:37:27 +0600

    filter with logical operator and array of objects

--- filter.js
+++ filter.js
@@ -1,5 +1,40 @@
-const coding = ['js','ruby','java','python','cpp'];
+const coding = ["js", "ruby", "java", "python", "cpp"];
 
-const myNumber = [1,2,3,4,5,6,7, 8, 9,10];
-const newNumber =  myNumber.filter( (num) => num > 4);
-console.log(newNumber);
+const myNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newNumber =  myNumber.filter( (num) => num > 4); //implicit return
+const newNumber = myNumber.filter((num) => {
+  // num > 4; // will return an empty array for not mentioning return keyword.
+  return num > 4; // {} for scope, use return keyword.
+});
+
+//condition based filter using forEach method
+//? code efficiency.
+const newArr = [];
+myNumber.forEach((number) => {
+  if (number > 4) {
+    newArr.push(number);
+  }
+});
+// console.log(newArr);
+
+//
+const books = [
+  {title: "Book One", genre: "Fiction", publish: 1981, edition: 2004,},
+  { title: "Book Two", genre: "Non-Fiction", publish: 1992, edition: 2008 },
+  { title: "Book Three", genre: "History", publish: 1999, edition: 2007 },
+  { title: "Book Four", genre: "Non-Fiction", publish: 1989, edition: 2010 },
+  { title: "Book Five", genre: "Science", publish: 2009, edition: 2014 },
+  { title: "Book Six", genre: "Fiction", publish: 1987, edition: 2010 },
+  { title: "Book Seven", genre: "History", publish: 1986, edition: 1996 },
+  { title: "Book Eight", genre: "Science", publish: 2011, edition: 2016 },
+  { title: "Book Nine", genre: "Non-Fiction", publish: 1981, edition: 1989 },
+];
+
+let userBooks = books.filter( (book) => {
+    return book.genre === "History";
+})
+
+userBooks = books.filter( (book) => {
+    return book.publish>=1995 && book.genre === "History";
+})
+console.log(userBooks);
