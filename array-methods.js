26cfb18e44b360f233fd921a5cb6ecf76844fd30
Tanha <nusrattanha897@gmail.com>
2024-02-05 23:41:15 +0600

    push,pop,shift,unshift

--- array-methods.js
+++ array-methods.js
@@ -1,27 +1,35 @@
 const myArr = [0,1,5,6,3,8,9];
 
 // accessing array elements using index
-// console.log(myArr[0]); // will show the first element in the array
+myArr[0]; // 0. will show the element which index is zero(0) in the array
 
 // basic methods of array in javascript
-// topic: push
-myArr.push(7);
+// topic: push - appends an element to the end and returns the new length of the array.
+//info: arr.push() and arr[arr.length] adds the new element.
+
+const newElement = myArr.push(7); 
+newElement; // length: 8
 myArr.push(10);
-// console.log(myArr);
+myArr; // [0, 1, 5,  6, 3, 8, 9, 7, 10]
 
-// topic: pop
-myArr.pop();
-// console.log(myArr);
+// topic: pop - extracts the last element and returns it.
+//info: arr.pop() and arr.at(-1)returns the last element but arr.pop() modifies the array by removing the element.
+
+const removedElement = myArr.pop();
+removedElement; //10
+myArr; //[0, 1, 5, 6, 3, 8, 9, 7]
 
 
-//? optimization problem for unshift and shift methods. not recommended
-// topic: unshift
-myArr.unshift(0);
-// console.log(myArr);
+//problem: optimization problem for unshift and shift methods. not recommended
+// topic: unshift : adds the element at the beginning and returns the new length of the array.
+const addedElement = myArr.unshift(50);
+addedElement; //9
+myArr; //[50, 0, 1, 5, 6, 3, 8, 9, 7]
 
-// topic: shift
-myArr.shift();
-// console.log(myArr);
+// topic: shift : extracts the first element and return it.
+const shiftElement = myArr.shift();
+shiftElement; // 50
+myArr; // [ 0, 1, 5, 6, 3, 8, 9, 7]
 
 // topic: includes
 // console.log(myArr.includes(9));
